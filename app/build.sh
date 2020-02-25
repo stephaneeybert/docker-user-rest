289d74660fd14a3653da4f1d070c38a194d42e26
Stephane Eybert <mittiprovence@yahoo.se>
2020-02-25 12:52:47 +0100

    Disabled the push to the remote server.

--- app/build.sh
+++ app/build.sh
@@ -5,5 +5,5 @@ if [ $? -eq 0 ]; then
 docker tag stephaneeybert/user-rest localhost:5000/user-rest
 docker push localhost:5000/user-rest
 docker tag stephaneeybert/user-rest thalasoft.com:5000/user-rest
-docker push thalasoft.com:5000/user-rest
+#docker push thalasoft.com:5000/user-rest
 fi
--- install/build.sh
+++ install/build.sh
@@ -5,7 +5,7 @@ if [ $? -eq 0 ]; then
 docker tag stephaneeybert/user-rest-mysql localhost:5000/user-rest-mysql
 docker push localhost:5000/user-rest-mysql
 docker tag stephaneeybert/user-rest-mysql thalasoft.com:5000/user-rest-mysql
-docker push thalasoft.com:5000/user-rest-mysql
+#docker push thalasoft.com:5000/user-rest-mysql
 fi
 #!/bin/bash
 
@@ -14,7 +14,7 @@ if [ $? -eq 0 ]; then
 docker tag stephaneeybert/user-rest-postgresql localhost:5000/user-rest-postgresql
 docker push localhost:5000/user-rest-postgresql
 docker tag stephaneeybert/user-rest-postgresql thalasoft.com:5000/user-rest-postgresql
-docker push thalasoft.com:5000/user-rest-postgresql
+#docker push thalasoft.com:5000/user-rest-postgresql
 fi
 
 docker build -t stephaneeybert/user-rest-install --rm=true .
@@ -22,5 +22,5 @@ if [ $? -eq 0 ]; then
 docker tag stephaneeybert/user-rest-install localhost:5000/user-rest-install
 docker push localhost:5000/user-rest-install
 docker tag stephaneeybert/user-rest-install thalasoft.com:5000/user-rest-install
-docker push thalasoft.com:5000/user-rest-install
+#docker push thalasoft.com:5000/user-rest-install
 fi
