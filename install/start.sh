e2365c7af3e707893b5867d40c3783e2f8ce33dc
Stephane Eybert <mittiprovence@yahoo.se>
2020-02-26 19:38:35 +0100

    Updated the Java version to the installed one.

--- install/start.sh
+++ install/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-export JAVA_HOME=/usr/local/jdk-12.0.1
+export JAVA_HOME=/usr/local/jdk-13.0.2
 export JAVA_PATH=$JAVA_HOME
 export JAVA_OPTS=""
 export CLASSPATH=.:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/jre/lib/rt.jar
